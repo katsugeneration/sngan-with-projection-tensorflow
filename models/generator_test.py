93bc560685dfdcecd0f052256e5f66bd42f1a66d
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-09-16 18:00:57 +0900

    Add generator train test case

--- models/generator_test.py
+++ models/generator_test.py
@@ -20,5 +20,20 @@ class SNGANGeneratorTest(tf.test.TestCase):
 
         self.assertEqual((N, o_H, o_H, 3), outputs.shape)
 
+    def testTrain(self):
+        N = 5
+        z_size = 10
+        z = tf.initializers.random_normal()((N, z_size))
+        sng = SNGANGenerator(category=5)
+        y = [[0]] * N
+
+        with tf.GradientTape() as tape:
+            outputs = sng(z, labels=y)
+            loss = tf.reduce_mean(tf.square(1 - outputs))
+
+        grads = tape.gradient(loss, sng.variables)
+        optimizer = tf.train.GradientDescentOptimizer(0.001)
+        optimizer.apply_gradients(zip(grads, sng.variables))
+
 
 tf.test.main()
