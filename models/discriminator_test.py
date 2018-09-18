38e4752f1f6130ff2c6ba3e33d41147f84181c16
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-09-19 08:40:32 +0900

    Add discriminator train test case

--- models/discriminator_test.py
+++ models/discriminator_test.py
@@ -19,6 +19,23 @@ class SNGANGeneratorTest(tf.test.TestCase):
 
         self.assertEqual((N, 1), outputs.shape)
 
+    def testTrain(self):
+        N = 5
+        H = 64
+        W = 64
+        C = 3
+        x = tf.ones((N, H, W, C))
+        snd = SNGANDiscriminator(category=5)
+        y = [[0]] * N
+
+        with tf.GradientTape() as tape:
+            outputs = snd(x, labels=y)
+            loss = tf.reduce_mean(tf.square(1 - outputs))
+
+        grads = tape.gradient(loss, snd.variables)
+        optimizer = tf.train.GradientDescentOptimizer(0.001)
+        optimizer.apply_gradients(zip(grads, snd.variables))
+
 
 if __name__ == '__main__':
     tf.enable_eager_execution()
